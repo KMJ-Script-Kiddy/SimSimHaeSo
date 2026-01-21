a56771a3d8d9eb83297249e85af287891f15e011
KMJ-Script-Kiddy <axtention@naver.com>
2026-01-21 17:30:40 +0900

    PS, sh, C 파일

---
+++ CLI/PowerShell/HashChecker_v3_사용자용.ps1
@@ -0,0 +1,23 @@
+﻿# 사용자 입력
+$FileLoc = Read-Host ">> 파일의 경로를 입력하세요"
+$FileExt = Read-Host ">> 파일의 확장자를 입력하세요"
+$Algorithm = Read-Host ">> 검사를 원하는 알고리즘을 입력하세요. 알고리즘: MACTripleDES|RIPEMD160|MD5|SHA512|SHA384|SHA256|SHA1
+"
+$InputHash = Read-Host ">> 원본 파일의 해시를 입력하세요($Algorithm)"
+
+# 파일
+$File = "$FileLoc\*.$FileExt"
+
+# 명령어 및 검사
+$OriginHash = (Get-FileHash $File -Algorithm $Algorithm).Hash
+
+# 검증
+if ($OriginHash -eq $InputHash) {
+    Write-Host "해시가 일치합니다. 검사가 완료되었습니다."
+} elseif ($FileLoc -eq "") {
+    Write-Host "경로를 입력하지 않아 오류가 발생하였습니다. 검사가 중지되었습니다."
+} elseif ($OriginHash -ne $InputHash) {
+    Write-Host "해시가 일치하지 않습니다. 파일의 무결성이 온전하지 않습니다."
+} else {
+    Write-Host "실행 중 오류가 발생하였습니다. 검사가 중지되었습니다."
+}
--- Language/C/arssh.c
+++ Language/C/arssh.c
@@ -3,94 +3,145 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define CONF "/etc/ssh/sshd_config"
-#define CONF_BAK "/etc/ssh/sshd_config.bak"
-#define AUTH_YES "PermitRootLogin yes"
-#define AUTH_NO "PermitRootLogin no"
+#define CONF_TMP "/tmp/sshd_config.tmp"
+#define PERMIT "PermitRootLogin"
+#define PERMIT_S "#PermitRootLogin"
 
 void backup() {
-	FILE *fp = fopen(CONF, "r");
+	time_t t = time(NULL);
+	struct tm *tm = gmtime(&t); // UTC 시간 참조
+
+	char ts[32];
+	if (strftime(ts, sizeof(ts), "%Y%m%d%H%M%S", tm) == 0) {
+		snprintf(ts, sizeof(ts), "unknown"); // 시간 참조 실패시
+	}
+
+	char CONF_BAK[128];
+	snprintf(CONF_BAK, sizeof(CONF_BAK), "/etc/ssh/sshd_config.bak_%s", ts); // 파일 이름 지정
 	
-}
-
-void auth_yes() {
-	FILE *fp = fopen(CONF, "r");
-	if (!fp) {
-		perror("파일이 없습니다.");
+	pid_t pid = fork();
+	if (pid == 0) { // 백업 파일 생성
+		execl("/bin/cp", "cp", CONF, CONF_BAK, NULL);
+		perror("[오류] 백업 파일 생성에 실패하였습니다");
 		exit(1);
 	}
-		
-	char line[256];
-	while (fgets(line, sizeof(line), fp)) {
-		if(strstr(line, AUTH_NO)) {
-			fprintf(fp, "%s\n", AUTH_YES);
-		} else {
-			fputs(line, fp);
-		}
-	}
 
-	fclose(fp);
-}
-
-void auth_no() {
-	FILE *fp = fopen(CONF, "r");
-	if (!fp) {
-		perror("파일이 없습니다.");
+	int status;
+	wait(&status);
+	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+		perror("[오류] 백업 파일 생성 중 오류가 발생하였습니다");
 		exit(1);
 	}
-		
-	char line[256];
-	while (fgets(line, sizeof(line), fp)) {
-		if(strstr(line, AUTH_YES)) {
-			fprintf(fp, "%s\n", AUTH_NO);
-		} else {
-			fputs(line, fp);
-		}
-	}
 
-	fclose(fp);
+	printf(">> 백업 파일을 생성하였습니다: %s\n", CONF_BAK);
+	fflush(stdout);
 }
 
-void restart_d() {
+void change_auth() {
+	FILE *src = fopen(CONF, "r");
+	FILE *dst = fopen(CONF_TMP, "w");
+	if (!src || !dst) {
+		perror("[오류] 파일을 여는 중 오류가 발생하였습니다");
+		exit(1);
+	}
+
+	char line[256];
+	char input[8];
+
+	do {
+		printf("변경할 설정을 입력하세요. [yes|no|prohibit-password]\n: ");
+		if (fgets(input, sizeof(input), stdin) == NULL) {
+			continue;
+		}
+		
+		input[strcspn(input, "\n")] = '\0';
+
+	} while (strcmp(input, "yes") != 0 && strcmp(input, "no") != 0 && strcmp(input, "prohibit-password") != 0);
+
+	while (fgets(line, sizeof(line), src)) { // src 파일을 줄 마다 읽으며 확인
+		if (strncmp(line, PERMIT, strlen(PERMIT)) == 0 || strncmp(line, PERMIT_S, strlen(PERMIT_S)) == 0)
+			continue; // PermitRootLogin 중복 제거
+		else
+			fputs(line, dst);
+	}
+	fprintf(dst, "%s %s\n", PERMIT, input);
+
+	if (fclose(src) != 0) {
+		perror("[오류] 원본 파일 닫기에 실패하였습니다");
+		exit(1);
+	}
+	
+	if (fclose(dst) != 0) {
+		perror("[오류] 임시 파일 닫기에 실패하였습니다");
+		exit(1);
+	}
+	
+	if (rename(CONF_TMP, CONF) != 0) { // 원본 파일로 붙여넣기
+		perror("[오류] 작성중인 파일을 원본으로 교체하는 데 실패하였습니다");
+		exit(1);
+	}
+}
+
+void restart() {
 	pid_t pid = fork();
 	if (pid == 0) {
-		execl("systemctl restart sshd");
-		return 0;
-	} else {
-		int status; wait(&status);
-		if (WIFEXITED(status)) {
-			printf("systemctl 종료 코드: %d\n" WEXITSTATUS(status));
-		}
+		execl("/bin/systemctl", "systemctl", "restart", "sshd", NULL);
+		perror("[오류] 데몬 재시작에 실패하였습니다");
+		exit(1);
 	}
+
+	int status;
+	wait(&status);
+	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+		perror("[오류] 데몬 재시작 중 오류가 발생하였습니다");
+		exit(1);
+	}
+
+	pid = fork();
+	if (pid == 0) {
+		execl("/bin/systemctl", "systemctl", "status", "sshd", NULL);
+		perror("[오류] 데몬 상태 확인에 실패하였습니다");
+		exit(1);
+	}
+
+	wait(NULL);
+
+	pid = fork();
+	if (pid == 0) {
+		execl("/bin/tail", "tail", CONF, NULL);
+		perror("[오류] 파일 읽기에 실패하였습니다");
+		exit(1);
+	}
+
+	wait(NULL);
 }
 
 int main() {
 	if (geteuid() != 0) {
-		fprintf(stderr, "관리자 권한으로만 실행 가능합니다.\n");
+		fprintf(stderr, "[오류] 관리자 권한으로만 실행 가능합니다.\n");
 		return 1;
 	}
 
 	int menu;
-	printf("1. Root 원격 접속 불허\n");
-	printf("2. Root 원격 접속 허용\n");
-	printf("메뉴를 선택하세요: ");
+	printf("1. Root 원격접속 상태 변경(사용자 입력)\n");
+	printf("2. 프로그램 종료\n");
+	printf(">> 메뉴를 선택하세요: ");
 	scanf("%d", &menu);
+
+	int c; while ((c = getchar()) != '\n' && c != EOF);
 
 	switch (menu) {
 		case 1:
-			auth_no();
-			printf("Root 원격 접속을 [불허]하였습니다.\n");
-			restart_d();
+			backup();
+			change_auth();
+			restart();
 			break;
 		case 2:
-			auth_yes();
-			printf("Root 원격 접속을 [허용]하였습니다.\n");
-			restart_d();
+			printf(">> 프로그램을 종료합니다.\n");
 			break;
-		default:
-			printf("잘못 선택하셨습니다.\n");
-			continue;
 	}
 
 	return 0;
